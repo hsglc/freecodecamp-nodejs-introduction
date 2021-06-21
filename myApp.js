f3586971f108142608ec10ace0ce731c2b0c1503
huseyinglc <huseyingulcuu@gmail.com>
2021-06-21 16:19:54 +0300

    fixed: Use method of express.

--- myApp.js
+++ myApp.js
@@ -3,10 +3,12 @@ var app = express();
 
 console.log("Hello World");
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use("public", express.static(__dirname + "/public"));
+
 
 module.exports = app;
