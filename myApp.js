8623a9999abf8a5020fb876f86067381be0f4d12
huseyinglc <huseyingulcuu@gmail.com>
2021-06-21 16:36:28 +0300

    fixed: adding json route

--- myApp.js
+++ myApp.js
@@ -5,7 +5,12 @@ console.log("Hello World");
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
-  res.json({ message: "Hello json" });
+});
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
 });
 
 app.use("/public", express.static(__dirname + "/public"));
