a431dece2a859d4711a4ea703c08cc4faf85cad6
huseyinglc <huseyingulcuu@gmail.com>
2021-06-21 15:28:31 +0300

    __dirname global variable was added to calculate absolute path

--- myApp.js
+++ myApp.js
@@ -4,8 +4,7 @@ var app = express();
 console.log("Hello World");
 
 app.get("/",function(req,res){
-    res.send('Hello Express');
-    res.sendFile(/views/index.html)
+    res.sendFile(__dirname + "views/index.html");
 })
 
  module.exports = app;
