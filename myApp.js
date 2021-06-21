c223f2dd25a15c77cafd17ff27e5f9b70f6ab2fc
huseyinglc <huseyingulcuu@gmail.com>
2021-06-21 16:31:27 +0300

    Json method added to get request to the /json route.

--- myApp.js
+++ myApp.js
@@ -3,14 +3,11 @@ var app = express();
 
 console.log("Hello World");
 
-
-
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
+  res.json({ message: "Hello json" });
 });
 
-app.use("/public",express.static(__dirname + "/public"));
-
-
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
